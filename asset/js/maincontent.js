1366ac49478d38151e59af0cfa16f987fe83f8b0
tester <wwltmdl@gmail.com>
2022-01-12 20:36:30 +0900

    side mini tab on/off & scroll bar

--- asset/js/maincontent.js
+++ asset/js/maincontent.js
@@ -1,7 +1,11 @@
 const rightIcon = document.querySelector('.topContent .right-icon');
-const contentSide = document.querySelector('.content-side');
+const contentSide = document.querySelector('.side-tab');
+const contentMiniSide = document.querySelector('.side-mini-tab');
+const contentMain = document.querySelector('.content-main');
 
 rightIcon.addEventListener('click',()=>{
     rightIcon.classList.toggle('active');
     contentSide.classList.toggle('active');
+    contentMain.classList.toggle('active');
+    contentMiniSide.classList.toggle('active');
 })
