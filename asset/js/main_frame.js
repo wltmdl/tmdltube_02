d2d4e8095dd9250692ad4d61c1d82848e2a4eab9
tester <wwltmdl@gmail.com>
2022-01-15 23:06:26 +0900

    main_video branch: second main_video width calc- jquery

---
+++ asset/js/main_frame.js
@@ -0,0 +1,13 @@
+//const theWidth = window.innerWidth;
+
+//$("#id이름을 누르면").submit(function(event){alert("dd")});
+
+$(window).resize(function(){
+    var winWidth = $(window).width();
+    var boxWidth = $('.video').width();
+
+    if(winWidth <= 1600 ){
+        $('.video').width(winWidth/5);
+        $('.video').height(winWidth/6);
+    }
+});
