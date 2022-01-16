72a5fa89a50b27e1292c090d40b7f9ec7bae550b
tester <wwltmdl@gmail.com>
2022-01-16 18:20:53 +0900

    finish: main-video @media and main_frame.js.file

--- asset/js/main_frame.js
+++ asset/js/main_frame.js
@@ -5,9 +5,15 @@
 $(window).resize(function(){
     var winWidth = $(window).width();
     var boxWidth = $('.video').width();
-
-    if(winWidth <= 1600 ){
+    
+    
+    
+ 
+    if(winWidth <= 1600 && winWidth > 1000 ){
         $('.video').width(winWidth/5);
         $('.video').height(winWidth/6);
+
     }
 });
+
+
--- asset/js/maincontent.js
+++ asset/js/maincontent.js
@@ -3,9 +3,14 @@ const contentSide = document.querySelector('.side-tab');
 const contentMiniSide = document.querySelector('.side-mini-tab');
 const contentMain = document.querySelector('.content-main');
 
+
 rightIcon.addEventListener('click',()=>{
+
     rightIcon.classList.toggle('active');
     contentSide.classList.toggle('active');
     contentMain.classList.toggle('active');
-    contentMiniSide.classList.toggle('active');
+    //contentMiniSide.classList.toggle('active');
+
+   
 })
+
